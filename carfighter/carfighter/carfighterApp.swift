afe1828f8c987a359b26a9585404129116334339
Cristian Scarano <cristianscarano20@gmail.com>
2024-12-17 17:17:51 +0100

    Added files.

---
+++ carfighter/carfighter/carfighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  carfighterApp.swift
+//  carfighter
+//
+//  Created by Cristian Scarano on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct carfighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
